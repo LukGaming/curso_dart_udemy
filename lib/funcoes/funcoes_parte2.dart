bf1fa607e9df30984bc3fde23139b968acedb669
Paulo <thelukgaming333@gmail.com>
2025-08-04 18:55:30 -0300

    Funcoes parte 2

---
+++ lib/funcoes/funcoes_parte2.dart
@@ -0,0 +1,30 @@
+String criarNomeCompleto(String nome, String sobrenome) {
+  return "$nome $sobrenome";
+}
+
+String informacoesUsuario({
+  required String nomeCompleto,
+  int? idade,
+  required int altura,
+  double peso = 70,
+}) {
+  return "Nome: $nomeCompleto, idade: $idade, altura: $altura, peso: $peso";
+}
+
+void main() {
+  final nome = "Paulo";
+  final sobrenome = "Mendes";
+
+  final nomeCompleto = criarNomeCompleto(nome, sobrenome);
+
+  print("nomeCompleto: $nomeCompleto");
+
+  final getInformacoesUsuario = informacoesUsuario(
+    nomeCompleto: nomeCompleto,
+    altura: 183,
+    idade: 27,
+    peso: 72,
+  );
+
+  print("Informações usuário: $getInformacoesUsuario");
+}
