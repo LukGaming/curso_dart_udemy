e3b4906c031d29d147fc2c005d3d40322c073684
Paulo <thelukgaming333@gmail.com>
2025-08-05 17:36:04 -0300

    Funções parte 3

---
+++ lib/funcoes/funcao_privada.dart
@@ -0,0 +1,11 @@
+void _funcaoPrivada() {
+  print("Função privada");
+}
+
+void funcaoPublica() {
+  print("Função publica");
+}
+
+void main() {
+  _funcaoPrivada();
+}
---
+++ lib/funcoes/funcoes_parte3.dart
@@ -0,0 +1,24 @@
+import 'package:curso_dart_udemy/funcoes/funcao_privada.dart';
+import 'package:curso_dart_udemy/funcoes/funcoes_parte2.dart';
+
+void main() {
+  final nomeCompleto = criarNomeCompleto("Paulo", "Mendes");
+
+  print("Nome completo: $nomeCompleto");
+
+  funcaoGlobal();
+
+  () {
+    print("Função anonima");
+  }.call();
+
+  funcaoPublica();
+}
+
+void funcaoGlobal() {
+  void funcaoEscopoPrivado() {
+    print("Hello world");
+  }
+
+  funcaoEscopoPrivado();
+}
