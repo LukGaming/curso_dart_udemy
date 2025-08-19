429bad8a8f1d8a4b31e9abe5b99f544023149022
Paulo <thelukgaming333@gmail.com>
2025-08-19 18:49:42 -0300

    Aula de IF e Switch

---
+++ lib/condicionais/switchs.dart
@@ -0,0 +1,28 @@
+void main() {
+  String statusPedido = "desconhecido";
+
+  switch (statusPedido) {
+    case "pendente":
+      print("Pedido pendente");
+      break;
+    case "producao":
+      print("Pedido em produção");
+      break;
+    case "finalizado":
+      print("Pedido finalizado");
+      break;
+    default:
+      print("Status desconhecido");
+  }
+
+  int numero = 1; //1,2,3
+
+  switch (numero) {
+    case >= 10:
+      print("Numero maior que 10");
+      break;
+    case < 10:
+      print("Numero menor que 10");
+      break;
+  }
+}
