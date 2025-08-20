944bb723b7ffab24a49fe47ea0f352af863849aa
Paulo <thelukgaming333@gmail.com>
2025-08-20 12:02:25 -0300

    Desafio dias semana

---
+++ lib/condicionais/desafio_condicionais_2.dart
@@ -0,0 +1,15 @@
+// Crie um programa que receba o número de um dia da semana (1 a 7) e imprima:
+
+// 1 → Segunda-feira
+
+// 2 → Terça-feira
+
+// 3 → Quarta-feira
+
+// 4 → Quinta-feira
+
+// 5 → Sexta-feira
+
+// 6 ou 7 → Final de semana
+
+// Qualquer outro número → "Dia inválido"
